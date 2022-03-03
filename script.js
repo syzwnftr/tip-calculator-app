a8bb9b3abc717103b1460d6307436805a5ecc937
Syazwan Fitri <syazfitry69@gmail.com>
2022-03-03 21:32:50 +0800

    Added function to make input field receives numbers and single dot only

--- script.js
+++ script.js
@@ -2,21 +2,34 @@ const inputBill = document.querySelector('#billAmount');
 const inputTip = document.querySelector('#customTip');
 const inputNumPeople = document.querySelector('#numOfPeople');
 
+// make an input only receives numbers and single dot(.) 
+function numbersDot(input) {
+    var num = /[^0-9\.]/g;
+    input.value = input.value.replace(num, '');
+    if(input.value.split('.').length > 2) {
+        input.value =input.value.replace(/\.+$/,"");
+    }
+}
+
+// make an input only receives numbers
 function numbersOnly(input) {
-    var num = /[^0-9]/gi;
+    var num = /[^0-9]/g;
     input.value = input.value.replace(num, '');
 }
 
 inputBill.addEventListener('input', () => {
-    numbersOnly(inputBill);
+    numbersDot(inputBill);
+    return parseFloat(inputBill.value);
 });
 
 inputTip.addEventListener('input', () => {
-    numbersOnly(inputTip);
+    numbersDot(inputTip);
+    return parseFloat(inputTip.value);
 });
 
 inputNumPeople.addEventListener('input', () => {
     numbersOnly(inputNumPeople);
+    return parseInt(inputNumPeople.value);
 });
 
 // reset input when page is refresh
