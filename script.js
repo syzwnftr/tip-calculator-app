d54b3fd821cdb3299a717584701dff8fbb07f7b9
Syazwan Fitri <syazfitry69@gmail.com>
2022-03-05 11:53:37 +0800

    Added function to change btnReset bg-color when input is filled

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const inputBill = document.querySelector('#billAmount');
 const inputTip = document.querySelector('#customTip');
 const inputNumPeople = document.querySelector('#numOfPeople');
+const btnReset = document.querySelector('#btnReset');
 
 // make an input only receives numbers and single dot(.) 
 function numbersDot(input) {
     var num = /[^0-9\.]/g;
     input.value = input.value.replace(num, '');
-    if(input.value.split('.').length > 2) {
+    if(input.value.split('.').length > 2) { // convert string into an array and check if array length is more than 2 
         input.value =input.value.replace(/\.+$/,"");
     }
 }
@@ -17,18 +18,30 @@ function numbersOnly(input) {
     input.value = input.value.replace(num, '');
 }
 
+// change btnReset bg-color when input is filled 
+function bgColorChange() {
+    if(inputBill.value || inputTip.value || inputNumPeople.value) {
+        btnReset.classList.add('got-input');
+    } else {
+        btnReset.classList.remove('got-input');
+    }
+}
+
 inputBill.addEventListener('input', () => {
     numbersDot(inputBill);
+    bgColorChange();
     return parseFloat(inputBill.value);
 });
 
 inputTip.addEventListener('input', () => {
     numbersDot(inputTip);
+    bgColorChange();
     return parseFloat(inputTip.value);
 });
 
 inputNumPeople.addEventListener('input', () => {
     numbersOnly(inputNumPeople);
+    bgColorChange();
     return parseInt(inputNumPeople.value);
 });
 
@@ -38,6 +51,7 @@ function resetInput() {
     inputTip.value = '';
     inputNumPeople.value = '';
 }
+
 
 resetInput();
     
